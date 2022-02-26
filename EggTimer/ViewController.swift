a7714b1ab99e967dfa09b71b98e5d8d231bc4d08
Chetan <chetan.getsmail@gmail.com>
2022-02-26 17:12:23 -0500

    Timer added on console

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var secondsRemaining = 60
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        print(sender.currentTitle ?? "afkldaf")
+        
+        let hardness = sender.currentTitle!
+        var secondsRemaining = eggTimes[hardness]
+        
+//        switch hardness {
+//        case eggTimes.values.description:
+//            print(eggTimes.keys)
+//        default:
+//            print("Error")
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true)
+        { (Timer) in
+            if secondsRemaining! > 0 {
+                   print ("\(secondsRemaining!) seconds")
+                secondsRemaining! -= 1
+               } else {
+                   Timer.invalidate()
+               }
+           }
+        }
+        
+    }
+    
 
-
-}
