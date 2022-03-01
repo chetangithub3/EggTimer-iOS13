cd84c2cb2b97783c4f6bd6140e4d4338cbd38807
Chetan <chetan.getsmail@gmail.com>
2022-03-01 00:24:36 -0500

    Audio

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+
 
 class ViewController: UIViewController {
-    
+    var player: AVAudioPlayer?
     @IBOutlet weak var titleChange: UILabel!
     
     @IBOutlet weak var progressBar: UIProgressView!
@@ -42,12 +46,33 @@ class ViewController: UIViewController {
                    Timer.invalidate()
                    self.titleChange.text = "Done"
                    self.progressBar.progress = 1.0
+                   self.playSound()
                    
-               }
            }
         }
         
     }
     
+    func playSound(){
+        let _:NSURL = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")! as NSURL
+        do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
 
+                /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+                
+
+                /* iOS 10 and earlier require the following line:
+                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = self.player else { return }
+
+                player.play()
+
+            } catch let error {
+                
+    }
+    }
+    
+}
 
